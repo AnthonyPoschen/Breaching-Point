027896aa6d1249822ab8e204f2ad9f796f3df730
Anthony Poschen <anthony_@live.com.au>
2014-07-03 23:08:13 +1000

    commiting new files

---
+++ Source/Breaching_Point/MinimapHUD.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "MinimapHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AMinimapHUD : public AHUD
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
